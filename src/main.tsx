7ba490bd7910e17085618373f9b4700629a87d94
CRooi <contact@crooi.io>
2025-02-07 02:05:24 +0800

    refactor(routes): remove file route

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,11 @@ import App from './App'
 import './global.css'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import File from './pages/file'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<App />} />
-            <Route path='/file' element={<File />} />
         </Routes>
     </BrowserRouter>
 )
