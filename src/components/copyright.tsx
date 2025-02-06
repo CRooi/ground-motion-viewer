4dcd1bc0afbc5a4edb6d0a080791fdd9480e72cf
CRooi <contact@crooi.io>
2025-02-06 19:52:25 +0800

    refactor(componentization): earthquake

--- src/components/copyright.tsx
+++ src/components/copyright.tsx
@@ -2,7 +2,7 @@ import { Container } from '@medusajs/ui'
 
 export default function Copyright({ className }: { className?: string }) {
     return (
-        <Container className={`text-right inline w-auto text-xs p-2 select-none bg-opacity-20 ${className}`}>
+        <Container className={`text-right inline w-auto text-xs p-2 select-none bg-opacity-20 text-black dark:text-white ${className}`}>
             <span>数据来源：中国地震台网中心预警速报部</span>
             {/* <br />
             <span>刘奕君、席楠、戴丹青、支明、蔡静等，2020年以来全国大陆M4.0级及以上地震强震动参数数据集，2024.</span> */}
